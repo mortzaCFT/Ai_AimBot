b960a89ec0ca973b5697c5c2415639a1fdcc90f9
mortzaCFT <79837660+mortzaCFT@users.noreply.github.com>
2024-02-09 15:57:18 +0330

    Update train_Objects.py

--- train_Objects.py
+++ train_Objects.py
@@ -4,12 +4,12 @@
 
 import torch
 
-model = torch.hup.load("ultralytics/yolov5, 'yolov5s', pretrained = True)
+model = torch.hup.load("ultralytics/yolov5", 'yolov5s', pretrained = True)
 
 # You should use your own images.
 # In this cheat, database is really valuble thing.
 # the database images also should have labels,and other matter stuff...
-images = ['','',''......]
+images = ['','',''...]
 
 results = model(images)
 
@@ -17,3 +17,18 @@ results.print()
 results.show()
 
 results.xyxy[0]
+
+# Also for train those images , you should know how to specifying dataset.
+# I used COCO128 train. example of coco128.yaml(this file should be on your dataset):
+
+# path : ../.../<the path of dataset root>
+# train: <name of your group images(like class)>
+# val: <name your value images.>
+
+# nc: <number of classes>
+# names: [ "<name of your objects>",...]
+
+
+
+# For training, you should run this commend on your device:
+# python train.py --img 640 --batch 16 --epochs 5 --data <name of your cocotrain.yaml file> --weights yolov5s.pt
