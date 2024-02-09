6899bd6476c640b9822361ac831434b1d49e791d
mortzaCFT <79837660+mortzaCFT@users.noreply.github.com>
2024-02-09 15:42:43 +0330

    Create train_Objects.py

---
+++ train_Objects.py
@@ -0,0 +1,4 @@
+# The main idea of this aim bot is using image procesing to analyze the video source real time,
+# And when the object detected, then program simulate a mouse click at the center of the object or other part of the object.
+# So, we need train ai, to create an pre trained weights using yolov5 to detect objets from source( i mean games.)
+
