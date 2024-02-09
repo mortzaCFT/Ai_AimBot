2cd1bda786440987b5634a4be03ab29789c873b2
mortzaCFT <79837660+mortzaCFT@users.noreply.github.com>
2024-02-09 15:47:44 +0330

    Update train_Objects.py

--- train_Objects.py
+++ train_Objects.py
@@ -2,3 +2,18 @@
 # And when the object detected, then program simulate a mouse click at the center of the object or other part of the object.
 # So, we need train ai, to create an pre trained weights using yolov5 to detect objets from source( i mean games.)
 
+import torch
+
+model = torch.hup.load("ultralytics/yolov5, 'yolov5s', pretrained = True)
+
+# You should use your own images.
+# In this cheat, database is really valuble thing.
+# the database images also should have labels,and other matter stuff...
+images = ['','',''......]
+
+results = model(images)
+
+results.print()
+results.show()
+
+results.xyxy[0]
